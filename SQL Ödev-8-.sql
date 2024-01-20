bcd8868ecd851d9f9982111fba73b59e092a7d3d
Berkan <152194536+BerkanSener@users.noreply.github.com>
2024-01-20 11:00:06 +0300

    Update SQL Ödev-8-.sql

--- SQL Ödev-8-.sql
+++ SQL Ödev-8-.sql
@@ -1,1 +1,73 @@
 
+-- 1. Soru test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+CRATE TABLE employee(
+    id INTEGER,
+    name VARCHAR(50),
+    birthday DATE,
+    email VARCHAR(100)
+)
+
+-- 2. Soru(1'in devamı) Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+insert into author (id, name, email, birthday) values (1, 'Davidsson', 'ldavidsson0@ezinearticles.com', '2024-01-03');
+insert into author (id, name, email, birthday) values (2, 'Gilbane', 'bgilbane1@wix.com', '2023-07-01');
+insert into author (id, name, email, birthday) values (3, 'Picardo', 'bpicardo2@comcast.net', '2023-12-03');
+insert into author (id, name, email, birthday) values (4, 'Caulton', 'tcaulton3@bbc.co.uk', '2023-03-19');
+insert into author (id, name, email, birthday) values (5, 'Twyning', 'ntwyning4@tripadvisor.com', '2023-10-22');
+insert into author (id, name, email, birthday) values (6, 'Solly', 'lsolly5@github.io', '2023-03-14');
+insert into author (id, name, email, birthday) values (7, 'Piwall', 'rpiwall6@tinyurl.com', '2024-01-19');
+insert into author (id, name, email, birthday) values (8, 'Cothey', 'acothey7@newsvine.com', '2023-03-16');
+insert into author (id, name, email, birthday) values (9, 'Asquez', 'nasquez8@answers.com', '2023-01-26');
+insert into author (id, name, email, birthday) values (10, 'Prenty', 'bprenty9@stanford.edu', '2023-11-19');
+insert into author (id, name, email, birthday) values (11, 'Delepine', 'hdelepinea@clickbank.net', '2023-12-05');
+insert into author (id, name, email, birthday) values (12, 'Riggey', 'briggeyb@soup.io', '2023-07-27');
+insert into author (id, name, email, birthday) values (13, 'Davenhill', 'mdavenhillc@shareasale.com', '2023-07-12');
+insert into author (id, name, email, birthday) values (14, 'Eirwin', 'peirwind@is.gd', '2023-05-21');
+insert into author (id, name, email, birthday) values (15, 'Giotto', 'jgiottoe@booking.com', '2023-03-27');
+insert into author (id, name, email, birthday) values (16, 'Kenrat', 'nkenratf@sourceforge.net', '2023-12-02');
+insert into author (id, name, email, birthday) values (17, 'Seak', 'mseakg@qq.com', '2023-03-10');
+insert into author (id, name, email, birthday) values (18, 'Macieiczyk', 'smacieiczykh@imgur.com', '2023-03-23');
+insert into author (id, name, email, birthday) values (19, 'O''Cannan', 'wocannani@slideshare.net', '2023-12-02');
+insert into author (id, name, email, birthday) values (20, 'Early', 'gearlyj@ted.com', '2023-11-10');
+insert into author (id, name, email, birthday) values (21, 'Jordan', 'djordank@dailymail.co.uk', '2023-10-20');
+insert into author (id, name, email, birthday) values (22, 'Kloss', 'aklossl@webeden.co.uk', '2023-02-09');
+insert into author (id, name, email, birthday) values (23, 'Doley', 'gdoleym@jigsy.com', '2023-06-15');
+insert into author (id, name, email, birthday) values (24, 'Lazenby', 'tlazenbyn@msn.com', '2023-04-29');
+insert into author (id, name, email, birthday) values (25, 'Ginnelly', 'wginnellyo@bandcamp.com', '2023-08-19');
+insert into author (id, name, email, birthday) values (26, 'Olensby', 'eolensbyp@blogger.com', '2023-08-22');
+insert into author (id, name, email, birthday) values (27, 'Robatham', 'crobathamq@dedecms.com', '2023-09-10');
+insert into author (id, name, email, birthday) values (28, 'Benedyktowicz', 'mbenedyktowiczr@free.fr', '2023-12-09');
+insert into author (id, name, email, birthday) values (29, 'Gurnee', 'pgurnees@unblog.fr', '2023-05-26');
+insert into author (id, name, email, birthday) values (30, 'Garretts', 'sgarrettst@microsoft.com', '2023-10-24');
+insert into author (id, name, email, birthday) values (31, 'Reaney', 'vreaneyu@businesswire.com', '2023-04-14');
+insert into author (id, name, email, birthday) values (32, 'Durrett', 'ldurrettv@squarespace.com', '2023-05-02');
+insert into author (id, name, email, birthday) values (33, 'Lubbock', 'jlubbockw@marketwatch.com', '2023-08-17');
+insert into author (id, name, email, birthday) values (34, 'Mattioli', 'rmattiolix@va.gov', '2023-11-13');
+insert into author (id, name, email, birthday) values (35, 'Spellacey', 'cspellaceyy@ucoz.ru', '2023-02-18');
+insert into author (id, name, email, birthday) values (36, 'Calcutt', 'scalcuttz@t.co', '2023-01-22');
+insert into author (id, name, email, birthday) values (37, 'Tancock', 'ltancock10@wix.com', '2023-02-04');
+insert into author (id, name, email, birthday) values (38, 'Ockwell', 'cockwell11@foxnews.com', '2023-12-23');
+insert into author (id, name, email, birthday) values (39, 'Charon', 'echaron12@dyndns.org', '2023-09-02');
+insert into author (id, name, email, birthday) values (40, 'Grinov', 'hgrinov13@github.com', '2023-04-17');
+insert into author (id, name, email, birthday) values (41, 'Ilyinykh', 'jilyinykh14@netvibes.com', '2023-11-10');
+insert into author (id, name, email, birthday) values (42, 'Kohnert', 'vkohnert15@sitemeter.com', '2023-09-08');
+insert into author (id, name, email, birthday) values (43, 'Govenlock', 'mgovenlock16@surveymonkey.com', '2023-09-02');
+insert into author (id, name, email, birthday) values (44, 'Reeman', 'areeman17@umich.edu', '2023-03-28');
+insert into author (id, name, email, birthday) values (45, 'Vost', 'cvost18@prnewswire.com', '2023-11-03');
+insert into author (id, name, email, birthday) values (46, 'Jiru', 'tjiru19@amazonaws.com', '2023-10-07');
+insert into author (id, name, email, birthday) values (47, 'Shepperd', 'dshepperd1a@illinois.edu', '2023-08-17');
+insert into author (id, name, email, birthday) values (48, 'Hadkins', 'ehadkins1b@people.com.cn', '2023-09-12');
+insert into author (id, name, email, birthday) values (49, 'Cawston', 'acawston1c@wikimedia.org', '2023-06-06');
+insert into author (id, name, email, birthday) values (50, 'Awty', 'jawty1d@nymag.com', '2023-09-21');
+
+-- 3. Soru Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+UPDATE employee
+SET name = 'Berkan',
+    birthday = '1992-11-02',
+	email = 'berkan@gmail.com'
+WHERE id = 12;	
+
+--4. Soru Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+DELETE FROM employee
+WHERE ind = 2;
+DELETE FROM employee
+WHERE birthday = '1995-05-04';
