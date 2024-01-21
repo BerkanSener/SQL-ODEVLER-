ffda096d2b3fc25105e81c98a0031ebb37c403e9
Berkan <152194536+BerkanSener@users.noreply.github.com>
2024-01-21 16:50:23 +0300

    Create SQL ÖDEV-11-.sql

---
+++ SQL ÖDEV-11-.sql
@@ -0,0 +1,29 @@
+-- 1. Soru actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer;
+
+-- 2. Soru actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+SELECT firt_name FROM actor
+INTERSECT
+SELECT firt_name FROM customer;
+
+-- 3. Soru actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
+
+--4. Soru İlk 3 sorguyu tekrar eden veriler için de yapalım.
+--
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer;
+
+--
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+--
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
